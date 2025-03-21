1c726a8a0e0987b38e1f99ac75a7005030169f50
logeshwari vijayakumar <logeshwarinisha16@gmail.com>
2025-03-21 20:02:17 +0530

    frontend
    bookstore

---
+++ Bookstore Front/.angular/cache/19.2.0/bookstore-front/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,39 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule
+} from "./chunk-ZHMFQYNE.js";
+import "./chunk-DEQVVKPS.js";
+import "./chunk-TAQY6HYA.js";
+import "./chunk-5RO6C5VN.js";
+import "./chunk-HDPSV3GH.js";
+import "./chunk-XCIYP5SE.js";
+import "./chunk-ZUJ64LXG.js";
+import "./chunk-OYTRG5F6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS
+};
